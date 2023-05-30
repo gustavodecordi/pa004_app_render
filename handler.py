6f269f0d96d9b983ee41f1d20b998e4a2c01bd4f
Gustavo <120114282+gustavodecordi@users.noreply.github.com>
2023-05-30 11:56:22 -0300

    Update handler.py, change the model

--- handler.py
+++ handler.py
@@ -5,7 +5,7 @@ from flask import Flask, request, Response
 from healthinsurance.HealthInsurance import HealthInsurance
 
 # loading model
-model = pickle.load( open( 'model/model_knn.pkl', 'rb' ) )
+model = pickle.load( open( 'model/model_linear_regression.pkl', 'rb' ) )
 
 # initialize API
 app = Flask( __name__ )
